98839536c0baa8741804c0549d0e3a55c2da23d5
Samriddhi <samriddhisrivastava.sr727@gmail.com>
2025-05-14 14:34:33 +0530

    [any message]

--- src/server.cpp
+++ src/server.cpp
@@ -1,65 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <cstring>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <unistd.h>
-#include <vector>
-using namespace std;
-struct DNS_header
-{
-    uint16_t ID;
-    uint16_t flags;
-    uint16_t QDCOUNT;
-    uint16_t ANCOUNT;
-    uint16_t NSCOUNT;
-    uint16_t ARCOUNT;
-};
-struct DNS_question
-{
-    string QNAME;
-    uint16_t QTYPE;
-    uint16_t QCLASS;
-};
-struct DNS_answer
-{
-    string NAME;
-    uint16_t TYPE;
-    uint16_t CLASS;
-    uint32_t TTL;
-    uint16_t RDLENGTH;
-    uint32_t RDATA;
-};
-struct DNS_message
-{
-    DNS_header header;
-    DNS_question question;
-    DNS_answer answers;
-};
-DNS_message make_message()
-{
-    DNS_message message;
-    message.header.ID = htons(1234);
-    message.header.flags = htons(1 << 15);
-    message.header.QDCOUNT = htons(1);
-    message.header.ANCOUNT = htons(1);
-    message.header.NSCOUNT = htons(0);
-    message.header.ARCOUNT = htons(0);
-    string s = "codecrafters";
-    message.question.QNAME += char(s.size());
-    message.question.QNAME += s;
-    s = "io";
-    message.question.QNAME += char(s.size());
-    message.question.QNAME += s;
-    message.question.QTYPE = htons(1);
-    message.question.QCLASS = htons(1);
-    message.answers.NAME = message.question.QNAME;
-    message.answers.TYPE = htons(1);
-    message.answers.CLASS = htons(1);
-    message.answers.TTL = htonl(60);
-    message.answers.RDLENGTH = htons(4);
-    message.answers.RDATA = htons(0x08080808);
-    return message;
-}
+#include "dns.hpp"
+#if _WIN32
+    typedef SOCKET Socket;
+    typedef int socklen_t;
+#else
+    typedef int Socket;
+#endif // _WIN32
 int main()
 {
     // Flush after every std::cout / std::cerr
@@ -67,16 +14,31 @@ int main()
     std::cerr << std::unitbuf;
     // Disable output buffering
     setbuf(stdout, NULL);
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    std::cout << "Logs from your program will appear here!" << std::endl;
+#if _WIN32
+    WSADATA wsaData;
+    int iResult;
+    iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (iResult != 0)
+    {
+        std::cerr << "WSA initialization failed: " << iResult << std::endl;
+        return 1;
+    }
+#endif
     // Uncomment this block to pass the first stage
-    int udpSocket;
-    udpSocket = socket(AF_INET, SOCK_DGRAM, 0);
+    Socket udpSocket;
+    struct sockaddr_in clientAddress;
+#if _WIN32
+    int protocol = IPPROTO_UDP;
+#else
+    int protocol = 0;
+#endif
+    udpSocket = socket(AF_INET, SOCK_DGRAM, protocol);
     if (udpSocket == -1)
     {
         std::cerr << "Socket creation failed: " << strerror(errno) << "..." << std::endl;
         return 1;
     }
+#if !_WIN32
     // Since the tester restarts your program quite often, setting REUSE_PORT
     // ensures that we don't run into 'Address already in use' errors
     int reuse = 1;
@@ -85,19 +47,22 @@ int main()
         std::cerr << "SO_REUSEPORT failed: " << strerror(errno) << std::endl;
         return 1;
     }
-    sockaddr_in serv_addr = {
-        .sin_family = AF_INET,
-        .sin_port = htons(2053),
-        .sin_addr = {htonl(INADDR_ANY)},
-    };
+#endif // !_WIN32
+    sockaddr_in serv_addr;
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(2053);
+#if _WIN32
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+#else
+    serv_addr.sin_addr = {htonl(INADDR_ANY)};
+#endif // _WIN32
     if (bind(udpSocket, reinterpret_cast<struct sockaddr *>(&serv_addr), sizeof(serv_addr)) != 0)
     {
         std::cerr << "Bind failed: " << strerror(errno) << std::endl;
         return 1;
     }
     int bytesRead;
-    char buffer[1024];
-    struct sockaddr_in clientAddress;
+    char buffer[512];
     socklen_t clientAddrLen = sizeof(clientAddress);
     while (true)
     {
@@ -109,31 +74,45 @@ int main()
             break;
         }
         buffer[bytesRead] = '\0';
-        std::cout << "Received " << bytesRead << " bytes: " << buffer << std::endl;
-        // Create an empty response
-        DNS_message response = make_message();
-        const uint16_t total_buffer_size = sizeof(response.header) + response.question.QNAME.size() + 5 + response.answers.NAME.size() + 10 + 5;
-        unsigned char *response_bytes = new unsigned char[total_buffer_size + 1];
-        memset(response_bytes, 0, total_buffer_size + 1);
-        memcpy(response_bytes, (void *)&response.header, 12);
-        auto ht = sizeof(response.header);
-        response.question.QNAME.copy((char *)(response_bytes + ht), response.question.QNAME.size());
-        memcpy(response_bytes + ht + response.question.QNAME.size() + 1, (void *)&response.question.QTYPE, 2);
-        memcpy(response_bytes + ht + response.question.QNAME.size() + 3, (void *)&response.question.QCLASS, 2);
-        ht = ht + response.question.QNAME.size() + 5;
-        response.answers.NAME.copy((char *)(response_bytes + ht), response.answers.NAME.size());
-        ht = ht + response.answers.NAME.size();
-        memcpy(response_bytes + ht + 1, (void *)&response.answers.TYPE, 2);
-        memcpy(response_bytes + ht + 3, (void *)&response.answers.CLASS, 2);
-        memcpy(response_bytes + ht + 5, (void *)&response.answers.TTL, 4);
-        memcpy(response_bytes + ht + 9, (void *)&response.answers.RDLENGTH, 2);
-        memcpy(response_bytes + ht + 11, (void *)&response.answers.RDATA, 4);
+        std::cout << "Received " << bytesRead << " bytes" << std::endl;
+        dns_packet_t request;
+        request.deserialize((u8*)buffer, bytesRead);
+        header_t request_header = request.get_header();
+        // Prepare response
+        dns_packet_t response;
+        header_t header;
+        header.id = request_header.id;
+        header.opcode = request_header.opcode;
+        header.rd = request_header.rd;
+        header.rcode = (request_header.opcode == 0 ? 0 : 4);
+        response.add_header(header);
+        auto label = request.get_labels();
+        response.add_question(label);
+        u32 ip = (8 << 24) | (8 << 16) | (8 << 8) | 8;
+        response.add_answer(request.get_labels(), ip);
+        // Print info about packet to be sent
+        if (false)
+        {
+            header_t response_header = response.get_header();
+            std::cout << "Sending id " << response_header.id
+                << " qdcount " << response_header.qdcount
+                << " qr " << (int)response_header.qr
+                << " labels " << response.get_labels()
+                << std::endl;
+        }
+        response.serialize();
         // Send response
-        if (sendto(udpSocket, response_bytes, total_buffer_size, 0, reinterpret_cast<struct sockaddr *>(&clientAddress), sizeof(clientAddress)) == -1)
+        std::cout << "Sending response..." << std::endl;
+        if (sendto(udpSocket, (char*)response.data(), response.size(), 0, reinterpret_cast<struct sockaddr *>(&clientAddress), sizeof(clientAddress)) == -1)
         {
             perror("Failed to send response");
         }
     }
+#if _WIN32
+    closesocket(udpSocket);
+    WSACleanup();
+#else
     close(udpSocket);
+#endif // _WIN32
     return 0;
 }
